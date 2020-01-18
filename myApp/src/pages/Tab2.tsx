43598d96e85f1a19974cc266e31cb8a992c5fc9c
sreesubbash <vostrosubbash97@hotmail.com>
2020-01-18 17:01:31 +0800

    Add Card

--- myApp/src/pages/Tab2.tsx
+++ myApp/src/pages/Tab2.tsx
@@ -1,7 +1,13 @@
 import React from 'react';
-import { IonContent, IonHeader, IonItem, IonLabel, IonList, IonPage, IonTitle, IonToolbar } from '@ionic/react';
+import { IonHeader, IonGrid, IonRow, IonCol, IonList, IonPage, IonTitle, IonToolbar } from '@ionic/react';
+import { IonButton, IonCard, IonCardContent, IonCardHeader, IonCardSubtitle, IonCardTitle, IonContent, IonIcon, IonItem, IonLabel } from '@ionic/react';
 
 const Tab2: React.FC = () => {
+
+  const like = () => {
+    console.log("hi");
+  };
+
   return (
     <IonPage>
       <IonHeader>
@@ -10,6 +16,32 @@ const Tab2: React.FC = () => {
         </IonToolbar>
       </IonHeader>
       <IonContent>
+        <IonCard>
+          <IonCardHeader>
+            <IonCardSubtitle>Card Subtitle</IonCardSubtitle>
+            <IonCardTitle>Card Title</IonCardTitle>
+          </IonCardHeader>
+
+          <IonCardContent>
+            Keep close to Nature's heart... and break clear away, once in awhile,
+            and climb a mountain or spend a week in the woods. Wash your spirit clean.
+            <div></div>
+            <IonGrid>
+              <IonRow>
+                <IonCol>
+                  <IonButton onClick={() => like()}expand="block" color="success">In</IonButton>
+                </IonCol>
+                <IonCol>
+                  <IonButton  expand="block" color="danger">Out</IonButton>
+                </IonCol>
+              </IonRow>
+            </IonGrid>
+
+
+
+          </IonCardContent>
+        </IonCard>
+
         <IonList>
           <IonItem routerLink="/tab2/details">
             <IonLabel>
