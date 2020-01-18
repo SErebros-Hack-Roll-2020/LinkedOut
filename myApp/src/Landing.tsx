d3a6f03a8a2c94fd6d7f9cabd91ec09f4d02b2af
sreesubbash <vostrosubbash97@hotmail.com>
2020-01-18 22:45:08 +0800

    Add landing page

--- myApp/src/Landing.tsx
+++ myApp/src/Landing.tsx
@@ -12,7 +12,7 @@ import {
 import { IonReactRouter } from '@ionic/react-router';
 import { apps, flash, send } from 'ionicons/icons';
 import Tab1 from './pages/Tab1';
-import login from './pages/login';
+import Login from './pages/login';
 import Tab2 from './pages/Tab2';
 import Tab3 from './pages/Tab3';
 import createPage from './pages/createPage';
@@ -37,35 +37,10 @@ import '@ionic/react/css/display.css';
 /* Theme variables */
 import './theme/variables.css';
 
-const App: React.FC = () => (
+const Landing: React.FC = () => (
   <IonApp>
-    <IonReactRouter>
-      <IonTabs>
-        <IonRouterOutlet>
-          <Route path="/tab1" component={Tab1} exact={true} />
-          <Route path="/create" component={createPage} exact={true} />
-          <Route path="/tab2" component={Tab2} exact={true} />
-          <Route path="/tab2/details" component={Details} />
-          <Route path="/tab3" component={Tab3} />
-          <Route path="/" render={() => <Redirect to="/tab1" />} exact={true} />
-        </IonRouterOutlet>
-        <IonTabBar slot="bottom">
-          <IonTabButton tab="tab1" href="/tab1">
-            <IonIcon icon={flash} />
-            <IonLabel>Tab One</IonLabel>
-          </IonTabButton>
-          <IonTabButton tab="tab2" href="/tab2">
-            <IonIcon icon={apps} />
-            <IonLabel>Tab Two</IonLabel>
-          </IonTabButton>
-          <IonTabButton tab="tab3" href="/tab3">
-            <IonIcon icon={send} />
-            <IonLabel>Tab Three</IonLabel>
-          </IonTabButton>
-        </IonTabBar>
-      </IonTabs>
-    </IonReactRouter>
+    <Login></Login>
   </IonApp>
 );
 
-export default App;
+export default Landing;
