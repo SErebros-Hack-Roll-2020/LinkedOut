fa2515bdbada72d762cacb62a325e1d8b74df024
Kwek Kee En <kwekkeeen@gmail.com>
2020-01-18 23:26:10 +0800

    merge

--- myApp/src/index.tsx
+++ myApp/src/index.tsx
@@ -3,47 +3,50 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-
 /* Firebase */
 import * as firebase from 'firebase/app';
 import 'firebase/auth';
 import { getConfig } from '@ionic/react';
 
-const firebaseConfig= {
-    apiKey: "",
-    authDomain: "linkedout-df9d4.firebaseapp.com",
-    databaseURL: "https://linkedout-df9d4.firebaseio.com",
-    projectId: "linkedout-df9d4",
-    storageBucket: "linkedout-df9d4.appspot.com",
-    messagingSenderId: "438683126903",
-    appId: "1:438683126903:web:0df9085a6ab2a5cfb00a72",
-    measurementId: "G-KJ4XBDKPFR"
-}
-firebase.initializeApp(firebaseConfig)
+const firebaseConfig = {
+    apiKey: 'AIzaSyB0iWQh9lOS5F9NsqN65y7chGxUJ_Fge8Q',
+    authDomain: 'linkedout-df9d4.firebaseapp.com',
+    databaseURL: 'https://linkedout-df9d4.firebaseio.com',
+    projectId: 'linkedout-df9d4',
+    storageBucket: 'linkedout-df9d4.appspot.com',
+    messagingSenderId: '438683126903',
+    appId: '1:438683126903:web:0df9085a6ab2a5cfb00a72',
+    measurementId: 'G-KJ4XBDKPFR'
+};
+firebase.initializeApp(firebaseConfig);
 
 var provider = new firebase.auth.GoogleAuthProvider();
-firebase.auth().onAuthStateChanged((user) => {
+firebase.auth().onAuthStateChanged(user => {
     if (user) {
         // user exists, do stuff
         ReactDOM.render(<App />, document.getElementById('root'));
     } else {
         // no user
-        firebase.auth().signInWithPopup(provider).then(function(result) {
-            // This gives you a Google Access Token. You can use it to access the Google API.
-            var credential = result.credential as firebase.auth.OAuthCredential
-            var token = credential.accessToken;
-            // The signed-in user info.
-            var user = result.user;
-            console.log(user)
-          }).catch(function(error) {
-            // Handle Errors here.
-            var errorCode = error.code;
-            var errorMessage = error.message;
-            // The email of the user's account used.
-            var email = error.email;
-            // The firebase.auth.AuthCredential type that was used.
-            var credential = error.credential;
-          });
+        firebase
+            .auth()
+            .signInWithPopup(provider)
+            .then(function(result) {
+                // This gives you a Google Access Token. You can use it to access the Google API.
+                var credential = result.credential as firebase.auth.OAuthCredential;
+                var token = credential.accessToken;
+                // The signed-in user info.
+                var user = result.user;
+                console.log(user);
+            })
+            .catch(function(error) {
+                // Handle Errors here.
+                var errorCode = error.code;
+                var errorMessage = error.message;
+                // The email of the user's account used.
+                var email = error.email;
+                // The firebase.auth.AuthCredential type that was used.
+                var credential = error.credential;
+            });
     }
 });
 
--- myApp/src/pages/Tab1.tsx
+++ myApp/src/pages/Tab1.tsx
@@ -1,21 +1,11 @@
 import {
-  IonCard,
-  IonCol,
-  IonButton,
-  IonCardContent,
-  IonCardHeader,
-  IonCardSubtitle,
-  IonCardTitle,
-  IonContent,
-  IonHeader,
-  IonIcon,
-  IonItem,
-  IonLabel,
-  IonList,
-  IonListHeader,
-  IonPage,
-  IonTitle,
-  IonToolbar
+    IonCard,
+    IonCol,
+    IonButton,
+    IonCardContent,
+    IonCardHeader,
+    IonCardSubtitle,
+    IonCardTitle,
 } from '@ionic/react';
 import { book, build, colorFill, grid } from 'ionicons/icons';
 import React, { useState, useEffect } from 'react';
@@ -36,50 +26,110 @@ const Tab1: React.FC = () => {
   };
 
   const [name, setName] = useState("name");
+        console.log('remove');
+    };
 
-  const getName = () => {
-    axios.get('/api/getName')
-      .then(response => {
-        console.log(response);
-        setName(response.data);
-      })
-  };
+    const [name, setName] = useState('name');
 
-  useEffect(() => {
-    getName();
-  }, []);
+    const getName = () => {
+        axios.get('/api/getName').then(response => {
+            console.log(response);
+            setName(response.data);
+        });
+    };
 
-  return (
-    <IonPage>
-      <IonHeader>
-        <IonToolbar>
-          <IonTitle>Profile</IonTitle>
-        </IonToolbar>
-      </IonHeader>
-      <IonContent>
-        <IonCard className="welcome-card">
-          <img src="/assets/icon/HumanIcon.png" alt="" />
-          <IonCardHeader>
-            <IonCardTitle>{name}</IonCardTitle>
-          </IonCardHeader>
-        </IonCard>
+    useEffect(() => {
+        getName();
+    }, []);
 
-        <IonList lines="none">
-          <IonListHeader>
-            <IonCol>
-              <IonButton onClick={() => settings()} color="success">Settings</IonButton>
-            </IonCol>
-            <IonCol>
-              <IonButton onClick={() => addMedia()} color="success">Add Media</IonButton>
-            </IonCol>
-            <IonCol>
-            <IonButton onClick={() => editInfo()} color="success">Edit info</IonButton>
-            </IonCol>
-          </IonListHeader>
-        </IonList>
-      </IonContent>
-    </IonPage>
-  );
+    const state = {
+        userData: { id: '1', name: 'Chen Hui', bio: 'D' },
+        groupData: [
+            { id: '1', name: 'Jason', bio: 'Developer' },
+            { id: '2', name: 'Subbash', bio: 'Designer' }
+        ]
+    };
+
+    const renderItems = () => {
+        const data = state.groupData;
+
+        const mapRows = data.map((item, index) => (
+            <IonCard>
+                <IonItem>
+                    <IonCol>
+                        <IonCardTitle>Name: {item.name}</IonCardTitle>
+                    </IonCol>
+                    <IonCol>
+                        <IonButton onClick={() => remove()} color="danger">
+                            Remove
+                        </IonButton>
+                    </IonCol>
+                </IonItem>
+                <IonCardContent> Bio: {state.userData.bio}</IonCardContent>
+            </IonCard>
+        ));
+        return mapRows;
+    };
+
+    return (
+        <IonPage>
+            <IonHeader>
+                <IonToolbar>
+                    <IonTitle>Profile</IonTitle>
+                </IonToolbar>
+            </IonHeader>
+            <IonContent>
+                <IonCard className="welcome-card">
+                    <img src="/assets/icon/HumanIcon.png" alt="" />
+                    <IonItem>
+                        <IonCol>
+                            <IonCardTitle>Name: {name}</IonCardTitle>
+                        </IonCol>
+                        <IonCol>
+                            <IonButton
+                                onClick={() => editInfo()}
+                                color="success"
+                            >
+                                Edit info
+                            </IonButton>
+                        </IonCol>
+                    </IonItem>
+                    <IonCardContent> Bio: {state.userData.bio}</IonCardContent>
+                </IonCard>
+
+                <IonList>{renderItems()}</IonList>
+
+                <IonList lines="none">
+                    <IonListHeader>
+                        <IonCol>
+                            <IonButton
+                                onClick={() => settings()}
+                                color="success"
+                            >
+                                Settings
+                            </IonButton>
+                        </IonCol>
+                        <IonCol>
+                            <IonButton
+                                onClick={() => addMedia()}
+                                color="success"
+                            >
+                                Add Media
+                            </IonButton>
+                        </IonCol>
+                        <IonCol>
+                            <IonButton
+                                onClick={() => editInfo()}
+                                color="success"
+                            >
+                                Edit info
+                            </IonButton>
+                        </IonCol>
+                    </IonListHeader>
+                </IonList>
+            </IonContent>
+        </IonPage>
+    );
 };
 
 export default Tab1;
